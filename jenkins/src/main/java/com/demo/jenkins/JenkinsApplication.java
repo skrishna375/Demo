036727351dde596761b028baf392afa8b3831ead
Krishna <skrishna375@gmail.com>
2018-12-05 01:11:11 +0530

    Updated new version with Docker

--- jenkins/src/main/java/com/demo/jenkins/JenkinsApplication.java
+++ jenkins/src/main/java/com/demo/jenkins/JenkinsApplication.java
@@ -2,13 +2,20 @@ package com.demo.jenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages= {"com.demo.jenkins"})
-public class JenkinsApplication {
+public class JenkinsApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsApplication.class, args);
 	}
+
+	 @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+      return builder.sources(JenkinsApplication.class);
+  }
 }
